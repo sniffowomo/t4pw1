e6088028973113fa9375e33ddc0a43e05a6a8b1d
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-01-13 04:45:59 +0000

    .

---
+++ lq/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspace/lq-postgres-1/lq/pnp.fish
+/workspace/lq-postgres-1/lq/fixrs.fish
+/workspace/lq-postgres-1/lq/st.sh
+/workspace/lq-postgres-1/lq/aryt.fish
+/workspace/lq-postgres-1/lq/tgpt.fish
+/workspace/lq-postgres-1/lq/bun.fish
+/workspace/lq-postgres-1/lq/tmuxx.fish
+
+
